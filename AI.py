832b2f4665ad37f763bb3c273f78ce2bd52b695a
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-09-21 16:05:43 +0530

    initial

---
+++ AI.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print("Hello " + name)
+
+task = input("What would you like me to do? ")
+
+if(task == 'nothing'):
+    print("Ight then")
+else:
+    print("Imma too lazy rn")
+    
---
+++ test.py
@@ -0,0 +1,8 @@
+a = "KSHITIJ"
+b = a.lower()
+x = "SHIT" in a
+y = "shit" in b
+if(x==1 or y==1):
+    print("True")
+else:
+    print("False")  
