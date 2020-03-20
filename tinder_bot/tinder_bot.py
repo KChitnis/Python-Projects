e266d4872ed436f3efb521a69ecaaafc254c0d3b
Kshitij Chitnis <47940274+KChitnis@users.noreply.github.com>
2020-03-20 23:51:10 +0530

    Update tinder_bot.py

--- tinder_bot/tinder_bot.py
+++ tinder_bot/tinder_bot.py
@@ -1,5 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 
-class TinderBot():
+class bot():
     def __init__(self):
-        self.driver = webdriver.Chrome('/usr/local/bin/')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('headless')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.get('www.tinder.com')
+
+    def loop(self):
+        while True:
+            self.driver.implicitly_wait(10)
+            sleep(2)
+            button1 = self.driver.find_element_by_xpath('//*[@id="poll-answer-2073"]')
+            button1.click()
+            sleep(1)
+            button2 = self.driver.find_element_by_xpath('//*[@id="polls-43-ans"]/p[1]/input')
+            button2.click()
+            sleep(1)
+            self.driver.refresh()
+
+bot = bot()
+bot.loop()
